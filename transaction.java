16a93b814fd34e7f5d66302bfd33068f6780e0d4
piaoxinai30 <49560996+piaoxinai30@users.noreply.github.com>
2019-09-06 19:14:09 +0900

    Create transaction.java

---
+++ transaction.java
@@ -0,0 +1,47 @@
+package kr.or.bit;
+
+public class Transaction {
+	private String transactionDate;
+	private String transactionTime;
+	private String kind;
+	private long amount;
+	private long balance;
+	
+	
+	
+	public String getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(String transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public String getTransactionTime() {
+		return transactionTime;
+	}
+	public void setTransactionTime(String transactionTime) {
+		this.transactionTime = transactionTime;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	public long getBalance() {
+		return balance;
+	}
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+	
+
+}
